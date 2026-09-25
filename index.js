4f6ef7445dae4b726b3e1ab859c435cfcb497f3a
Antmnn1 <antoniomanna435@gmail.com>
2026-09-25 10:24:24 +0200

    Finished About

--- index.js
+++ index.js
@@ -11,71 +11,116 @@ let cardnews2 = new CardNews('27.03 ARTISTS REVEALED', 'Meet the artists shaping
 let cardnews3 = new CardNews('16.12 FESTIVAL DATES', 'Mark your calendar: Partenope Festival begins on May 3rd. A new edition of contemporary visual arts is ready to unfold, bringing together artists, ideas, and experiences across the city. Discover the programme, explore the venues, and join us as Partenope comes to life.')
 let cardnews4 = new CardNews('01.01 OPEN CALLS', 'Partenope Festival open calls are now open. From January 1st to January 31st, artists and creatives are invited to submit their work and become part of the festival. Discover the call, explore the requirements, and take your chance to contribute to the next edition of Partenope.')
 
-let contenitore = document.querySelector('.swiper-wrapper')
+let contenitore = document.querySelector('#due .swiper-wrapper')
 let cards = [cardnews1, cardnews2, cardnews3, cardnews4]
 
-for (let card of cards) {
-    let div = document.createElement('div')
-    div.classList.add('swiper-slide')
-    div.innerHTML = `
-                            <div class="cardNews">
+if (contenitore) {
+
+    for (let card of cards) {
+        let div = document.createElement('div')
+        div.classList.add('swiper-slide')
+        div.innerHTML = `
                                 <div class="cardNews">
-                                    <p class="titoloNews m-0">${card.dataTitolo}</p>
-                                    <p class="contentNews m-0">${card.contenuto}</p>
+                                    <div class="cardNews">
+                                        <p class="titoloNews m-0">${card.dataTitolo}</p>
+                                        <p class="contentNews m-0">${card.contenuto}</p>
+                                    </div>
                                 </div>
-                            </div>
-    `
-    contenitore.append(div)
+        `
+        contenitore.append(div)
+    }
+
+    let primaSlide = contenitore.querySelector('.swiper-slide')
+
+    if (primaSlide) {
+        primaSlide.id = 'primaSlideNews'
+    }
 }
 
-let primaSlide = contenitore.querySelector('.swiper-slide')
-primaSlide.id = 'primaSlideNews'
 
 //CLASSE E ISTANZE TICKETS IN HOME
 class TicketsHome {
-    constructor(dataTitolo, nomeEvento, artisti, prezzo,img) {
+    constructor(dataTitolo, nomeEvento, artisti, prezzo, img) {
         this.dataTitolo = dataTitolo
         this.nomeEvento = nomeEvento
         this.artisti = artisti
         this.prezzo = prezzo
-        this.img=img
+        this.img = img
     }
 }
 
-let ticket1 = new TicketsHome('03.05 WORKSHOP','Visual Identity Lab','Luca Moretti / Sofia Van Dijk',25,'img/card_ticket1.webp')
-let ticket2 = new TicketsHome('03.05 TALK','Designing new Perspectives','Arthur Bellamy / Lina Kovacs / Eva Rossetti','FREE','img/card_ticket2.webp')
-let ticket3 = new TicketsHome('04.05 INSTALLATION','Sonic Territories','Aya Nakamura',10,'img/card_ticket3.webp')
+let ticket1 = new TicketsHome('03.05 WORKSHOP', 'Visual Identity Lab', 'Luca Moretti / Sofia Van Dijk', 25, 'img/card_ticket1.webp')
+let ticket2 = new TicketsHome('03.05 TALK', 'Designing new Perspectives', 'Arthur Bellamy / Lina Kovacs / Eva Rossetti', 'FREE', 'img/card_ticket2.webp')
+let ticket3 = new TicketsHome('04.05 INSTALLATION', 'Sonic Territories', 'Aya Nakamura', 10, 'img/card_ticket3.webp')
 
-let tickets =[ticket1,ticket2,ticket3]
+let tickets = [ticket1, ticket2, ticket3]
 let contenitoreT = document.querySelector('#appendTickets')
 
-for(let ticket of tickets){
-    let div = document.createElement('div')
-    div.classList.add('d-flex')
-    div.id = 'contenitoreTickets'
-    div.innerHTML=`
-     <div class="imgTicket">
-                        <img src="${ticket.img}" alt="" class="w-100">
-                    </div>
-                    <div class="Ticket">
-                        <div class="redBar d-flex justify-content-center align-items-center">
-                            <p>${ticket.dataTitolo}</p>
+if (contenitoreT) {
+
+    for (let ticket of tickets) {
+        let div = document.createElement('div')
+        div.classList.add('d-flex')
+        div.id = 'contenitoreTickets'
+        div.innerHTML = `
+         <div class="imgTicket">
+                            <img src="${ticket.img}" alt="" class="w-100">
                         </div>
-                        <div class="bodyTicket">
-                            <div class="d-flex flex-column justify-content-center align-items-center">
-                                <h3>${ticket.nomeEvento}</h3>
-                                <p>${ticket.artisti}</p>
+                        <div class="Ticket">
+                            <div class="redBar d-flex justify-content-center align-items-center">
+                                <p>${ticket.dataTitolo}</p>
+                            </div>
+                            <div class="bodyTicket">
+                                <div class="d-flex flex-column justify-content-center align-items-center">
+                                    <h3>${ticket.nomeEvento}</h3>
+                                    <p>${ticket.artisti}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="plusTicket d-flex flex-column">
-                        <div class="d-flex justify-content-center align-items-center">
-                            <p>${ticket.prezzo === 'FREE' ? 'FREE' : ticket.prezzo + ' €'}</p>
+                        <div class="plusTicket d-flex flex-column">
+                            <div class="d-flex justify-content-center align-items-center">
+                                <p>${ticket.prezzo === 'FREE' ? 'FREE' : ticket.prezzo + ' €'}</p>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center">
+                                <img src="img/plus.svg" alt="" width="72px" height="72px">
+                            </div>
                         </div>
-                        <div class="d-flex align-items-center justify-content-center">
-                            <img src="img/plus.svg" alt="" width="72px" height="72px">
-                        </div>
-                    </div>
-    `
-    contenitoreT.append(div)
+        `
+        contenitoreT.append(div)
+    }
+}
+
+
+//CLASSE E ISTANZE GALLERY IN ABOUT
+class CardGallery {
+    constructor(img, data) {
+        this.img = img
+        this.data = data
+    }
+}
+
+let cardgallery1 = new CardGallery('img/unoEdPre.webp', '08.05.2025')
+let cardgallery2 = new CardGallery('img/dueEdPre.webp', '08.05.2025')
+let cardgallery3 = new CardGallery('img/treEdPre.webp', '07.05.2025')
+let cardgallery4 = new CardGallery('img/quattroEdPre.webp', '06.05.2025')
+let cardgallery5 = new CardGallery('img/cinqueEdPre.webp', '06.05.2025')
+let cardgallery6 = new CardGallery('img/seiEdPre.webp', '05.05.2025')
+
+let cardsGallery = [cardgallery1, cardgallery2, cardgallery3, cardgallery4, cardgallery5, cardgallery6]
+
+let contGallery = document.querySelector('#galleryPrecedenti .swiper-wrapper')
+
+if (contGallery) {
+
+    for (let card of cardsGallery) {
+        let div = document.createElement('div')
+        div.classList.add('swiper-slide')
+        div.innerHTML = `
+                                <div class="cardGallery">
+                                    <img src="${card.img}" alt="">
+                                    <p>${card.data}</p>
+                                </div>
+        `
+        contGallery.append(div)
+    }
 }
